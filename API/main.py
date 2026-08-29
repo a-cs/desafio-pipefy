af33720ce8c0a7bed42509d4d9c8265a8fb2ce16
a-cs <acarneirosousa@gmail.com>
2026-08-28 23:25:16 -0300

    creating fuction to_pipefy_fields to generate the fields for the graphql query

--- API/main.py
+++ API/main.py
@@ -1,11 +1,31 @@
 from datetime import date, datetime
+import sys
 from typing import Any, Literal, Type, TypeAlias, Annotated, Union
 from fastapi import FastAPI, Request, status
 from fastapi.middleware.cors import CORSMiddleware
 from fastapi.exceptions import RequestValidationError
 from fastapi.responses import JSONResponse
 from pydantic import BaseModel, Field, field_validator, RootModel, model_validator, BeforeValidator, PlainSerializer, WithJsonSchema, StringConstraints
+from dotenv import load_dotenv
+import os
 
+def env_initialization(): 
+	load_dotenv()
+	required_envs = ["PIPEFY_API_URL", "PIPEFY_TOKEN", "PIPE_ID"]
+	missing_envs: list[str] = []
+
+	for env in required_envs:
+		if not os.getenv(env):
+			missing_envs.append(env)
+
+	if missing_envs:
+		print(f"Erros: Variáveis de ambiente faltando: {', '.join(missing_envs)}")
+		print("Preencha o arquivo .env antes de iniciar.")
+		sys.exit(1)
+
+	print("Variáveis de ambiente carregadas com sucesso!")
+
+env_initialization()
 
 def create_type_from_list_of_options(valid_options: list[str], field: str) -> Any:
 	# Creating a customized Pydantic typy from a list
@@ -32,16 +52,6 @@ sexo_options= [
 				"Prefere não responder"
 				]
 
-cidade_options = [
-					"Maracanaú",
-					"Maranguape",
-					"Eusébio",
-					"Caucaia",
-					"Fortaleza",
-					"fortaleza",
-					"Florianópolis"
-					]
-
 valid_hobbies: TypeAlias = Literal[
 					"Teatro",
 					"Música",
@@ -51,6 +61,18 @@ valid_hobbies: TypeAlias = Literal[
 					"Viagem",
 					"Artes"
 					]
+
+cidades_id={
+	"Maracanaú": "850256388",
+	"Maranguape": "850256601",
+	"Eusébio": "850256710",
+	"Caucaia": "850256822",
+	"Fortaleza": "850256930",
+	"fortaleza": "1150166576",
+	"Florianópolis": "1340824346",
+}
+
+cidade_options = list(cidades_id.keys())
 
 class list_hobbies(RootModel[list[valid_hobbies]]):
 	@model_validator(mode="after")
@@ -62,40 +84,41 @@ class list_hobbies(RootModel[list[valid_hobbies]]):
 # 1.Factory method to create validatos for the BR date and datetime format
 def dates_br_factory(expected_type: Type[Union[date, datetime]]) -> Any:
 	is_datetime = expected_type is datetime
-	formats = ["%d/%m/%Y %H:%M"] if is_datetime else ["%d/%m/%Y"]
+	format = "%d/%m/%Y %H:%M" if is_datetime else "%d/%m/%Y"
 	error_msg = "DD/MM/YYYY HH:MM" if is_datetime else "DD/MM/YYYY"
 
-	def validator(value: Any) -> Any:
+	def validator(value: Any) -> str:
+		if hasattr(value, "strftime"):
+			return value.strftime(format)
 		if isinstance(value, str):
-			for format in formats:
-				try:
-					dt = datetime.strptime(value, format)
-					return dt if is_datetime else dt.date()
-				except ValueError:
-					continue
-			raise ValueError(f"Formato de data inválido. Use o padrão {error_msg}.")
+			try:
+				dt = datetime.strptime(value, format)
+				return dt.strftime(format)
+			except ValueError:
+				raise ValueError(f"Formato de data inválido. Use o padrão {error_msg}.")
 		raise ValueError(f"O campo deve ser uma string no formato {error_msg}.")
-
+	
 	def serializer(value: Any) -> str:
-		output_format = "%d/%m/%Y %H:%M" if isinstance(value, datetime) else "%d/%m/%Y"
-		return value.strftime(output_format)
+		if hasattr(value, "strftime"):
+			return value.strftime(format)
+		return str(value)
 
 	return validator, serializer, error_msg
 
-validate_date, format_date, date_example = dates_br_factory(date)
-validate_datetime, format_datetime, datetime_example = dates_br_factory(datetime)
+validate_date, serialize_date, date_example = dates_br_factory(date)
+validate_datetime, serialize_datetime, datetime_example = dates_br_factory(datetime)
 
 DateBR = Annotated[
-	date,
+	str,
 	BeforeValidator(validate_date),
-	PlainSerializer(format_date, return_type=str),
+	PlainSerializer(serialize_date, return_type=str, when_used="always"),
 	WithJsonSchema({"type": "string", "example": date_example})
 	]
 
 DatetimeBR = Annotated[
-	datetime,
+	str,
 	BeforeValidator(validate_datetime),
-	PlainSerializer(format_datetime, return_type=str),
+	PlainSerializer(serialize_datetime, return_type=str, when_used="always"),
 	WithJsonSchema({"type": "string", "example": datetime_example})
 	]
 
@@ -120,6 +143,42 @@ def validate_telefone_br(value: Any) -> str:
 
 TelefoneBr = Annotated[str,BeforeValidator(validate_telefone_br)]
 short_text = Annotated[str, StringConstraints(max_length=255)]
+
+class CardData(BaseModel):
+	nome: short_text #requidred true
+	data_de_nascimento: DateBR | None = None #requidred false 
+	cpf: CPF | None = None #requidred false
+	telefone: TelefoneBr | None = None #requidred false
+	data: DatetimeBR | None = None #requidred false
+	sexo: str | None = None #requidred false
+	sexo_validator = field_validator("sexo", mode="after")(create_type_from_list_of_options(sexo_options, "sexo"))
+	cidade: str #requidred true
+	cidade_validator = field_validator("cidade", mode="after")(create_type_from_list_of_options(cidade_options, "cidade"))
+	hobbies: list_hobbies | None = None #requidred false
+
+	@property
+	def cidade_id(self) -> str | None:
+		return cidades_id.get(self.cidade)
+
+
+	def to_pipefy_fields(self) -> list[dict[str, str]]:
+		fields_attributes: list[dict[str, str]] = []
+
+		for field_name in self.__dict__.keys():
+			if field_name == "cidade":
+				value = self.cidade_id
+			else:
+				value = getattr(self, field_name)
+				value = getattr(value, "root", value)
+			if value is None:
+				continue
+
+			fields_attributes.append({
+				"field_id": field_name,
+				"field_value": str(value)
+			})
+
+		return fields_attributes
 
 
 app = FastAPI()
@@ -185,19 +244,9 @@ async def validation_exception_handler(request: Request, exc: RequestValidationE
 		},
 	)
 
-
-
-class CardData(BaseModel):
-	nome: short_text #requidred true
-	data_de_nascimento: DateBR | None = None #requidred false 
-	cpf: CPF | None = None #requidred false
-	telefone: TelefoneBr | None = None #requidred false
-	data: DatetimeBR | None = None #requidred false
-	sexo: str | None = None #requidred false
-	sexo_validator = field_validator("sexo", mode="after")(create_type_from_list_of_options(sexo_options, "sexo"))
-	cidade: str #requidred true
-	cidade_validator = field_validator("cidade", mode="after")(create_type_from_list_of_options(cidade_options, "cidade"))
-	hobbies: list_hobbies | None = None #requidred false
+pipefy_api_url = os.getenv("PIPEFY_API_URL")
+pipefy_token = os.getenv("PIPEFY_TOKEN")
+pipe_id = os.getenv("PIPE_ID")
 
 @app.get("/")
 def greeting():
@@ -205,9 +254,35 @@ def greeting():
 
 @app.post("/card")
 def create_card(card: CardData) -> dict[str, Any]:
-	if card.data == None:
-		card.data = datetime.now()
-	print(card)
+	card_title = card.nome
+	pipefy_fields = card.to_pipefy_fields()
+
+	query = """
+	mutation ($pipe_id: ID!, $title: String!, $fields_attributes: [UndefinedInput]) {
+		createCard(input: {
+		pipe_id: $pipe_id,
+		title: $title,
+		fields_attributes: $fields_attributes
+		}) {
+		card {
+			id
+			title
+		}
+		}
+	}
+	"""
+
+	variables: dict[str, str | list[dict[str, str]] | None] = {
+		"pipe_id": pipe_id,
+		"title": card_title,
+		"fields_attributes": pipefy_fields
+	}
+
+	headers = {
+		"Authorization": f"Bearer {pipefy_token}",
+		"Content-Type": "application/json"
+	}
+
 	return {
-		"received_data": card
+		"received_data": card.to_pipefy_fields()
 	}
--- README.md
+++ README.md
@@ -1,3 +1,13 @@
+## Para setar as variaveis de ambiente
+- Entre na pasta /API
+- Abra o terminal na pasta
+- No terminal execute o comando para criar o arquivo ".env" com base no arquivo ".env.example":
+  ```shell
+  cp .env.example .env
+  ```
+- Abrar o arquivo ".env"
+- Digita os valores das variáveis de ambiente
+
 ## Como ativar a api
 - Entre na pasta /API
 - Abra o terminal na pasta
